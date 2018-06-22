bfba5d3eb89acbe477a398fc038a9e00e5505bd0
Mustakim Ali <mustakimali@hotmail.com>
2018-06-22 22:50:10 +0100

    Added AppInsight

--- src/JustSending/Program.cs
+++ src/JustSending/Program.cs
@@ -12,6 +12,7 @@ namespace JustALink
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
--- src/JustSending/Startup.cs
+++ src/JustSending/Startup.cs
@@ -56,6 +56,7 @@ namespace JustALink
             {
                 routes.MapHub<ConversationHub>("/signalr/hubs");
             });
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
