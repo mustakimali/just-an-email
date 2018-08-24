26449da520fbf63bd56cd5ac0a75f2e2582904c7
Mustakim Ali <mustakimali@hotmail.com>
2018-08-24 01:15:30 +0100

    Simple Integration test
    Integration test and no unit tests sound like a bad idea, yes it is. Ask me if you are curious to find out why I am doing this?

--- test/JustSending.Test/IntegrationTest.cs
+++ test/JustSending.Test/IntegrationTest.cs
@@ -1,7 +1,7 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JustSending;
 using System.Linq;
 using System;
+using System.Threading;
 using Microsoft.AspNetCore.TestHost;
 using System.Net.Http;
 using JustALink;
@@ -11,14 +11,17 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using System.Reflection;
 using System.IO;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Diagnostics;
+using NUnit.Framework;
 
 namespace JustSending.Test
 {
-    [TestClass]
+    [TestFixture]
     public class IntegrationTest
     {
-        private readonly TestServer _server;
-        private readonly HttpClient _client;
+        private readonly string _seleniumDriverPath;
 
         public IntegrationTest()
         {
@@ -26,20 +29,54 @@ namespace JustSending.Test
             var relativePath = "../../../../../src/JustSending".Replace('/', Path.DirectorySeparatorChar);
             var contentRoot = Path.GetFullPath(relativePath, basePath);
 
-            _server = new TestServer(WebHost.CreateDefaultBuilder()
-                    .UseApplicationInsights()
-                    .UseContentRoot(contentRoot)
-                    .UseStartup<Startup>()
-                    .UseEnvironment("Development"));
-            _client = _server.CreateClient();
+            _seleniumDriverPath = Path.Combine(Path.GetFullPath("../../../", basePath), "Drivers");
         }
 
-        [TestMethod]
-        public async Task TempTest()
+        [Test]
+        public void Integration_1()
         {
-            var stats = await _client.GetStringAsync("http://localhost");
+            using (var client1 = CreateDriver())
+            using (var client2 = CreateDriver())
+            {
+                client1.FindElement(By.Id("new-session")).Click();
 
-            stats.Should().Contain("Mustakim Ali");
+                client2.FindElement(By.Id("connect")).Click();
+
+                WaitMs(1000);
+
+                var token = client1.FindElement(By.Id("token")).Text;
+
+                client2.FindElement(By.Id("Token")).SendKeys(token);
+                client2.FindElement(By.Id("connect")).Click();
+
+                WaitMs(1000);
+
+                client2.FindElement(By.Id("ComposerText")).SendKeys(Guid.NewGuid().ToString("N"));
+                client2.FindElement(By.ClassName("sendBtn")).Click();
+
+                WaitMs(1000);
+
+                client1.FindElement(By.ClassName("navbar-brand")).Click();
+                WaitMs(500);
+                client2.FindElement(By.ClassName("confirm")).Click();
+                WaitMs(500);
+                client2.FindElement(By.ClassName("navbar-brand")).Click();
+            }
+        }
+
+        private IWebDriver CreateDriver()
+        {
+            var driver = new ChromeDriver(_seleniumDriverPath);
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl("http://localhost:61452");
+
+            return driver;
+        }
+
+        private void WaitMs(int milliseconds)
+        {
+            System.Threading.Thread.Sleep(milliseconds);
         }
     }
 }
